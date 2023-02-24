ae038af64809dca90ff1eddd1fae2e2da4def4c2
David Arno <david@davidarno.org>
2023-02-24 22:47:04 +0000

    Now have working parametized tests for valid configs

--- tests/test_spritesheet_reader_with_valid_images.py
+++ tests/test_spritesheet_reader_with_valid_images.py
@@ -1,31 +1,14 @@
 import pytest
 from stylesheet2python import SpriteSheetConfiguration, SpriteSheet, RGB
-#from PIL.Image import Image  #type: ignore
 from PIL import Image  #type: ignore
 
-# need to do some tests. Parametized is the way to go I think. Can loop through combinations of dynamic/1+ values
-# for height and width, all combos of options and various widths that force stop pattern to be on lines 1-3
-# (4+? - probably not. If works for 2-3, will work for n)
-
-#@pytest.mark.parametrize("in_data, out_data", [(3, 5), (3, 4)])
-#def test_answer(in_data: int, out_data: int):
-#    assert func(in_data) == out_data
-
-# image widths 9, 17, 35, 50
-# sprite widths 1, 2, 5, 8, (11, 15)
-# sprite heights 1, 3, 7, 10, 19, 31, 48, 60
-# control black/colour
-# blanks_between_rows black/colour
-# blanks_between_sprites black/colour
-#
-# So 4 x 6 x 8 x 2 x 2 x 2 = 1500ish tests
 BLACK = RGB(0, 0, 0)
-CONTROL = RGB(11, 15, 17)
-ROW_BLANK = RGB(31, 71, 97)
-SPRITE_BLANK = RGB(111, 171, 13)
+CONTROL = RGB(211, 115, 17)
+ROW_BLANK = RGB(31, 171, 97)
+SPRITE_BLANK = RGB(111, 71, 113)
 
 IMAGE_WIDTHS = [9, 17, 35, 50]
-IMAGE_HEIGHT = 10
+IMAGE_HEIGHT = 11
 SPRITE_WIDTHS = [None, 1, 2, 5, 8, 11, 15]
 SPRITE_HEIGHTS = [None, 1, 3, 7, 10, 19, 31, 48, 60]
 CONTROL_BITS = [None, CONTROL]
@@ -42,12 +25,12 @@ def create_image(
         sprite_blank: RGB|None) -> Image.Image:
 
     def next_colour(r: int, g: int, b: int) -> tuple[int, int, int]:
-        if b >= 254 and g >= 254:
-            return (r + 2, 0, 0)
-        elif b >= 254:
-            return (r, g + 2, 0)
+        if b >= 246 and g >= 246:
+            return (r + 10, 0, 0)
+        elif b >= 246:
+            return (r, g + 10, 0)
         else:
-            return (r, g, b +2)
+            return (r, g, b + 10)
 
     def set_pixel_get_next_colour(image: Image.Image, x: int, y: int, r: int, g: int, b: int) -> tuple[int, int, int]:
         image.putpixel((x, y), (r, g, b))
@@ -56,7 +39,7 @@ def create_image(
     def update_xy(x: int, y: int) -> tuple[int, int]:
         x += 1
         if x >= image_width:
-            x = 0
+            x = x - image_width
             y += 1
 
         return (x, y)
@@ -107,7 +90,7 @@ def create_image(
     (x, y) = draw_pixel_get_new_xy_and_rgb(image, x, y, row_blank)
     (x, y) = draw_pixel_get_new_xy_and_rgb(image, x, y, control_bit)
 
-    if x + 8 >= image_width:
+    if x + 7 >= image_width:
         y += 1
 
     x = image_width - 8
@@ -134,8 +117,80 @@ def create_configuration(
         control_bit is not None,
         control_bit)
 
-def file_based_test_data() -> list[tuple[Image.Image, SpriteSheetConfiguration]]:
-    test_data = []
+def create_test_name(
+        image_width: int,
+        image_height: int,
+        sprite_width: int|None,
+        sprite_height: int|None,
+        control_bit: RGB|None,
+        row_blank: RGB|None,
+        sprite_blank: RGB|None) -> str:
+
+    def format_size(value: int|None) -> str|int:
+        if value is None:
+            return "<d>"
+        else:
+            return value
+
+    def format_blank(value: RGB|None) -> str:
+        if value is None:
+            return "n"
+        else:
+            return "<y>"
+
+    def format_control(value: RGB|None) -> str:
+        if value is None:
+            return "n"
+        else:
+            return "<s>"
+
+    return (
+        f"i:{image_width}x{image_height}_s{format_size(sprite_width)}x{format_size(sprite_height)}_"
+        f"c:{format_control(control_bit)}_rb:{format_blank(row_blank)}_sb:{format_blank(sprite_blank)}"
+    )
+
+def create_image_config_and_name(
+        image_width: int,
+        image_height: int,
+        sprite_width: int|None,
+        sprite_height: int|None,
+        control_bit: RGB|None,
+        row_blank: RGB|None,
+        sprite_blank: RGB|None) -> tuple[Image.Image, SpriteSheetConfiguration, str]:
+
+    image = create_image(
+        image_width,
+        image_height,
+        sprite_width,
+        sprite_height,
+        control_bit,
+        row_blank,
+        sprite_blank
+    )
+
+    config = create_configuration(
+        sprite_width,
+        sprite_height,
+        control_bit,
+        row_blank,
+        sprite_blank
+    )
+
+    name = create_test_name(
+        image_width,
+        image_height,
+        sprite_width,
+        sprite_height,
+        control_bit,
+        row_blank,
+        sprite_blank
+    )
+
+    return image, config, name
+
+def generate_test_data_and_names() -> tuple[list[tuple[Image.Image, SpriteSheetConfiguration]], list[str]]:
+    images_and_configurations = []
+    names = []
 
     for image_width in IMAGE_WIDTHS:
         for sprite_width in SPRITE_WIDTHS:
@@ -146,7 +201,7 @@ def file_based_test_data() -> list[tuple[Image.Image, SpriteSheetConfiguration]]
                 for control_bit in CONTROL_BITS:
                     for row_blank in ROW_BLANK_BITS:
                         for sprite_blank in SPRITE_BLANK_BITS:
-                            image, config = create_image_and_config(
+                            image, config, name = create_image_config_and_name(
                                 image_width,
                                 IMAGE_HEIGHT,
                                 sprite_width,
@@ -156,49 +211,15 @@ def file_based_test_data() -> list[tuple[Image.Image, SpriteSheetConfiguration]]
                                 sprite_blank
                             )
 
-                            test_data.append((image, config))
+                            images_and_configurations.append((image, config))
+                            names.append(name)
 
-    return test_data
+    return images_and_configurations, names
 
-def test_0():
-    rgb_image = create_image(9, 5, None, 12, None, RGB(155, 1, 221), RGB(155, 1, 221))
+IMAGES_AND_CONFIGURATIONS, NAMES = generate_test_data_and_names()
+
+@pytest.mark.parametrize(("rgb_image", "expected_config"), IMAGES_AND_CONFIGURATIONS, ids=NAMES)
+def test_sprite_config(rgb_image, expected_config):
     spritesheet = SpriteSheet(rgb_image)
     actual_config = spritesheet.configuration
-    expected_config = create_configuration(None, 12, None, RGB(155, 1, 221), RGB(155, 1, 221))
     assert expected_config == actual_config
-
-def test_1():
-    image = Image.open("C:\\PersonalDev\\spritesheet-to-python\\tests\\images\\valid_40_h1_w8_s156-0-220.png")
-    rgb_image = image.convert('RGB')
-    spritesheet = SpriteSheet(rgb_image)
-    config = spritesheet.configuration
-    assert config.sprite_height == 1
-    assert config.sprite_width == 8
-    assert config.control_pixel_colour == RGB(156, 0, 220)
-    assert config.strict_mode
-    assert config.blanks_between_rows
-    assert config.blanks_between_sprites
-
-def test_2():
-    image = Image.open("C:\\PersonalDev\\spritesheet-to-python\\tests\\images\\valid_30x2_h10_w9_s255-255-255.png")
-    rgb_image = image.convert('RGB')
-    spritesheet = SpriteSheet(rgb_image)
-    config = spritesheet.configuration
-    assert config.sprite_height == 10
-    assert config.sprite_width == 9
-    assert config.control_pixel_colour == RGB(255, 255, 255)
-    assert config.strict_mode
-    assert not config.blanks_between_rows
-    assert not config.blanks_between_sprites
-
-def test_3():
-    image = Image.open("C:\\PersonalDev\\spritesheet-to-python\\tests\\images\\valid_40_no-h_no-w_no-s.png")
-    rgb_image = image.convert('RGB')
-    spritesheet = SpriteSheet(rgb_image)
-    config = spritesheet.configuration
-    assert config.sprite_height is None
-    assert config.sprite_width is None
-    assert config.control_pixel_colour is None
-    assert not config.strict_mode
-    assert not config.blanks_between_rows
-    assert not config.blanks_between_sprites
